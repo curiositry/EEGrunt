e12001dc98f1c7b0f99378fe5efacf8e2bc58f00
Omphalosskeptic <isa@automathic.org>
2015-06-15 12:06:33 -0700

    Add sample block, plot destination, NFFT etc to config.py
    Former-commit-id: b3db849379469b9ea453c7ecfc51beda591dbcbc

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ config = {
     't_lim_sec': [0, 3300.0],
     'nchannels': 8,
     'channels': [1,2,3,4,5,6,7,8],
-    'channel': 8
+    'channel': 8,
+    'sample_block': 100,
+    'plot': 'save'
     }
