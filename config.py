689d010206fa8438f657d70380daef43c93da96c
Omphalosskeptic <isa@automathic.org>
2015-06-15 15:38:58 -0700

    Monkeying with random stuff, testing on CSV and .txt files
    Former-commit-id: 69ff8fac249ba70579bd1e605e666a0aa9749e74

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ config = {
     'fs_Hz': 250.0,
     'NFFT': 256*2,
     'path': 'data/',
-    # 'filename': 'OpenBCI-RAW-2015-04-23_08-46-58-rf-meditation2.txt',
-    'filename': 'EEG_data.csv',
+    'filename': 'OpenBCI-RAW-2015-04-23_08-46-58-rf-meditation2.txt',
+    # 'filename': 'EEG_data.csv',
     't_lim_sec': [0, 3300.0],
     'nchannels': 8,
     'channels': [1,2,3,4,5,6,7,8],
     'channel': 8,
     'sample_block': 100,
     'plot': 'show',
-    'band': [13, 30, "Beta"]
+    'band': [7, 12, "Alpha"]
     }
