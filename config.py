a89e97e19c82707ea317d135d60a35873242e2b5
Omphalosskeptic <isa@automathic.org>
2015-06-15 10:41:01 -0700

    Add config.py, for variables the casual user needs to update
    Former-commit-id: 541d37e3d491ac4de229f9598bf870b8f49eed7e

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+    'fs_Hz': 250.0,
+    'path': 'data/',
+    'filename': 'OpenBCI-RAW-2015-04-23_08-46-58-rf-meditation2.txt',
+    't_lim_sec': [0, 3300.0],
+    'nchannels': 8,
+    'channels': [1,2,3,4,5,6,7,8],
+    'channel': 8
+    }
