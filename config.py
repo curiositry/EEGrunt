68058b35207285e0360ece0355e10a008e91a2c5
Omphalosskeptic <isa@automathic.org>
2015-06-15 14:32:17 -0700

    Add band config var, switch to show plots
    Former-commit-id: cccd3478a041a3762f59cc679b1ace246eb3b16c

--- config.py
+++ config.py
@@ -2,11 +2,12 @@ config = {
     'fs_Hz': 250.0,
     'NFFT': 256*2,
     'path': 'data/',
-    'filename': 'OpenBCI-RAW-2015-04-23_08-46-58-rf-meditation2.txt',
+    'filename': 'OpenBCI-RAW-js-eeg-3.txt',
     't_lim_sec': [0, 3300.0],
     'nchannels': 8,
     'channels': [1,2,3,4,5,6,7,8],
     'channel': 8,
     'sample_block': 100,
-    'plot': 'save'
+    'plot': 'show',
+    'band': [8, 12]
     }
