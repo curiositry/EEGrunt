dd61c9b9881a99b052510c063d445fa6050b1808
Omphalosskeptic <isa@automathic.org>
2015-06-18 18:52:28 -0700

    Now with coherence fft and auto fileformat skipcols
    Former-commit-id: e421205e8d3b3ce4a996c21d9c5e06fc484b0cd2

--- openbci_analysis.py
+++ openbci_analysis.py
@@ -1,8 +1,5 @@
 from config import *
 from EEGrunt_functions import *
-from scipy.signal import butter, lfilter, freqz
-from scipy import signal
-from scipy.interpolate import spline
 
 # Set shorter vars from config vars (transitional)
 fs_Hz     = config['fs_Hz']
@@ -11,21 +8,26 @@ t_lim_sec = config['t_lim_sec']
 nchannels = config['nchannels']
 channels  = config['channels']
 channel   = config['channel']
+overlap  = config['NFFT'] - int(0.25 * config['fs_Hz'])
 activity  = "meditation"
+skiprows = 5
+if(config['filename'][(len(config['filename']) - 3):] == "csv"):
+    skiprows = 2
 
 # Add more if you want to generate plots for different section of an experiment
 data_segments = [
     {
-    "start_time":0 ,
-    "end_time":3600,
+    "start_time":50 ,
+    "end_time":100,
     "title":config['filename'],
     "type":activity
     }]
+    
 
 # Load up the numpy array
 raw_data = np.loadtxt(config['path'] + config['filename'],
 	          delimiter=',',
-	          skiprows=2,
+	          skiprows=skiprows,
 	          usecols=(0,1,2,3,4,5,6,7,8)
 	          )
               
@@ -36,6 +38,7 @@ t_sec = np.arange(len(raw_data[:, 0])) / fs_Hz
 for segment_id in range(len(data_segments)):
     start = data_segments[segment_id]["start_time"]
     end = data_segments[segment_id]["end_time"]
+    segment_duration = end - start
     data_segments[segment_id]["data"] = raw_data[(fs_Hz*start):(fs_Hz*end), 1:(nchannels+1)]
 
 # Check packets
@@ -47,6 +50,8 @@ for segment_id in range(len(data_segments)):
     seginfo = data_segments[segment_id]
     data = raw_data[:, channel:]
     data = seginfo["data"][:,(channel-1)]
+    seg_t = np.arange(len(data)) / fs_Hz
+
     
     # Set default plot title
     title = "Channel "+str(channel)+"\n"+seginfo["title"]
@@ -57,7 +62,7 @@ for segment_id in range(len(data_segments)):
     data = bandpass(unbandpassed_data, config['band'])
 
     # Make Spectrogram 
-    spectrogram(data, title)
+    #spectrogram(data, title)
 
     # Convert things for trend graph (for trend graph)
     spec_PSDperBin, freqs, t_spec = get_spec_psd_per_bin(data)
@@ -67,7 +72,7 @@ for segment_id in range(len(data_segments)):
     avgd_data = avg_samples(band_max_uVperSqrtBin)   
     
     # Make trend graph     
-    plot_amplitude_over_time(full_t_spec[::config['sample_block']], smooth(avgd_data[:len(avgd_data)-10:]), title)
+    #plot_amplitude_over_time(full_t_spec[::config['sample_block']], smooth(avgd_data[:len(avgd_data)-10:]), title)
     
     # Convert things for FFT
     # hz_data = data - np.mean(unbandpassed_data,0)
@@ -78,4 +83,8 @@ for segment_id in range(len(data_segments)):
                                 noverlap=overlap
                                ) 
     # Plot FFT
-    plot_spectrum_avg_fft(spec_PSDperHz,freqs2,title)
+    #plot_spectrum_avg_fft(spec_PSDperHz,freqs2,title)
+    
+    s1 = bandpass(seginfo["data"][:,1-1], config['band'])
+    s2 = bandpass(seginfo["data"][:,8-1], config['band'])
+    plot_coherence_fft(s1,s2,title,"1","8")
