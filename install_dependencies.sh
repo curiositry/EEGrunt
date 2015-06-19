d97ca27e499f9f6343b30d00c9111f91e7334fca
Omphalosskeptic <isa@automathic.org>
2015-06-18 18:51:11 -0700

    Update ... you'll need to build scipy from source to get the cohere functions'
    Former-commit-id: b613c63018836e88da10c5f45cf9a081f20fca08

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,1 +1,4 @@
-sudo apt-get install python-pip python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+sudo apt-get install python-pip python-dev build-essential libatlas-base-dev gfortran
+sudo pip install numpy
+sudo pip install scipy
+sudo apt-get install  python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
