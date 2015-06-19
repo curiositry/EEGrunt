fddea2d9982c8249972bdc4e772564e1dce5b9df
Omphalosskeptic <isa@automathic.org>
2015-06-18 18:51:24 -0700

    Update
    Former-commit-id: 29da4ed5178464e74f8cbcbaa7140eba77e28427

--- config.py
+++ config.py
@@ -2,12 +2,13 @@ config = {
     'fs_Hz': 250.0,
     'NFFT': 256*2,
     'path': 'data/',
-    'filename': 'EEG_data.csv',
+    # 'filename': 'EEG_data.csv',
+    'filename': 'OpenBCI-RAW-2015-04-23_08-46-58-rf-meditation2.txt',
     't_lim_sec': [0, 3300.0],
     'nchannels': 8,
     'channels': [1,2,3,4,5,6,7,8],
     'channel': 8,
     'sample_block': 100,
     'plot': 'show',
-    'band': [8.0, 12.0, "Alpha"]
+    'band': [0, 50, "Alpha"]
     }
