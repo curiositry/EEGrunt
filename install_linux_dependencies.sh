5d13baf250fab7ef61c1e7ead94284e58a79999a
Curiositry <isa@automathic.org>
2016-08-05 16:53:02 -0700

    Update install_linux-dependencies.sh (comment code + condense to two commands)

--- install_linux_dependencies.sh
+++ install_linux_dependencies.sh
@@ -1,4 +1,10 @@
-sudo apt-get install python-pip python-dev build-essential libatlas-base-dev gfortran
-sudo pip install numpy
-sudo pip install scipy
-sudo apt-get install  python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+#!/bin/sh
+#
+# Install Linux Dependencies
+# ==========================
+# This little script will automatically install all the packages needed to run
+# EEGrunt on (ubuntu) Linux, if you're lucky.
+
+sudo apt-get install python python-pip python-dev build-essential libatlas-base-dev gfortran python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+
+sudo pip install scipy numpy
