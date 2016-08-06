c040f7731c399814854fe4e946f0cf5713da60e2
Curiositry <isa@automathic.org>
2016-08-05 17:02:30 -0700

    Add plot_ecg function, which plots RR intervals, and will form the basis for heart-rate + HRV analysis. Also, parted plotit function out into two functions. Note: This commit mostly authored by Photosynthesis <adam@photosynth.ca>.

--- EEGrunt.py
+++ EEGrunt.py
@@ -7,7 +7,7 @@ from scipy import signal
 
 class EEGrunt:
     def __init__(self, path, filename, source, title = ""):
-    
+
         self.path = path
         self.filename = filename
         self.source = source
@@ -15,7 +15,7 @@ class EEGrunt:
             self.session_title = title
         else:
             self.session_title = source.title()+" data loaded from "+filename
-        
+
         if self.source == 'openbci' or self.source == 'openbci-openvibe':
             self.fs_Hz = 250.0
             self.NFFT = 256*2
@@ -23,21 +23,21 @@ class EEGrunt:
             self.channels = [1,2,3,4,5,6,7,8]
             self.col_offset = 0
 
-        
+
         if self.source == 'muse':
             self.fs_Hz = 220.0
             self.NFFT = 220*2
             self.nchannels = 4
             self.channels = [1,2,3,4]
             self.col_offset = -1
-            
-            
+
+
         self.sample_block = 11
-        
+
         self.plot = 'show'
-        
+
         self.overlap  = self.NFFT - int(0.25 * self.fs_Hz)
-        
+
 
 
 
@@ -46,9 +46,9 @@ class EEGrunt:
         path = self.path
         filename = self.filename
         source = self.source
-        
+
         print("Loading EEG data: "+path+filename)
-        
+
         try:
             with open(path+filename) as file:
                 pass
@@ -87,18 +87,18 @@ class EEGrunt:
 
 
         self.raw_data = raw_data
-        
+
         self.t_sec = np.arange(len(self.raw_data[:, 0])) /self.fs_Hz
 
 
-    
+
     def load_channel(self,channel):
         print("Loading channel: "+str(channel))
         channel_data = self.raw_data[:,(channel+self.col_offset)]
         self.channel = channel
         self.data = channel_data
 
-        
+
     def packet_check(self):
         data_indices = self.data[:, 0]
         d_indices = data_indices[2:]-data_indices[1:-1]
@@ -108,30 +108,29 @@ class EEGrunt:
 
     def remove_dc_offset(self):
         hp_cutoff_Hz = 1.0
-        
+
         print("Highpass filtering at: " + str(hp_cutoff_Hz) + " Hz")
-        
-        b, a = signal.butter(2, hp_cutoff_Hz/(self.fs_Hz / 2.0), 'highpass') 
+
+        b, a = signal.butter(2, hp_cutoff_Hz/(self.fs_Hz / 2.0), 'highpass')
         self.data = signal.lfilter(b, a, self.data, 0)
 
-        
+
     def notch_mains_interference(self):
         notch_freq_Hz = np.array([60.0])  # main + harmonic frequencies
         for freq_Hz in np.nditer(notch_freq_Hz):  # loop over each target freq
-            bp_stop_Hz = freq_Hz + 3.0*np.array([-1, 1])  # set the stop band       
-            b, a = signal.butter(3, bp_stop_Hz/(self.fs_Hz / 2.0), 'bandstop') 
+            bp_stop_Hz = freq_Hz + 3.0*np.array([-1, 1])  # set the stop band
+            b, a = signal.butter(3, bp_stop_Hz/(self.fs_Hz / 2.0), 'bandstop')
             self.data = signal.lfilter(b, a, self.data, 0)
             print("Notch filter removing: " + str(bp_stop_Hz[0]) + "-" + str(bp_stop_Hz[1]) + " Hz")
-            
+
     def bandpass(self,start,stop):
         bp_Hz = np.zeros(0)
         bp_Hz = np.array([start,stop])
         b, a = signal.butter(3, bp_Hz/(self.fs_Hz / 2.0),'bandpass')
         print("Bandpass filtering to: " + str(bp_Hz[0]) + "-" + str(bp_Hz[1]) + " Hz")
         return signal.lfilter(b, a, self.data, 0)
-         
-        
-    def smooth(x,window_len=11,window='hanning'):
+
+    def smooth(self,x,window_len=11,window='hanning'):
         if x.ndim != 1:
             raise ValueError, "smooth only accepts 1 dimension arrays."
         if x.size < window_len:
@@ -148,14 +147,18 @@ class EEGrunt:
         y=np.convolve(w/w.sum(),s,mode='valid')
         return y
 
-    def plotit(self,plt, filename=""):
+    def plotit(self, plt, filename=""):
         if self.plot == 'show':
-            plt.show()
-            plt.close()
+            plt.draw()
         if self.plot == 'save':
             plt.savefig(filename)
             plt.close()
-        
+
+    def showplots(self):
+        if self.plot == 'show':
+            print("Computation complete! Showing generated plots...")
+            plt.show()
+
     def signalplot(self):
         print("Generating signal plot...")
         plt.figure(figsize=(10,5))
@@ -165,7 +168,7 @@ class EEGrunt:
         plt.ylabel('Power (uV)')
         plt.title(self.plot_title('Signal'))
         self.plotit(plt)
-        
+
     def get_spectrum_data(self):
         print("Calculating spectrum data...")
         self.spec_PSDperHz, self.spec_freqs, self.spec_t = mlab.specgram(np.squeeze(self.data),
@@ -175,8 +178,8 @@ class EEGrunt:
                                        noverlap=self.overlap
                                        ) # returns PSD power per Hz
         # convert the units of the spectral data
-        self.spec_PSDperBin = self.spec_PSDperHz * self.fs_Hz / float(self.NFFT) 
-    
+        self.spec_PSDperBin = self.spec_PSDperHz * self.fs_Hz / float(self.NFFT)
+
 
     def spectrogram(self):
         print("Generating spectrogram...")
@@ -199,20 +202,20 @@ class EEGrunt:
             backgroundcolor='w')
         self.plotit(plt, self.plot_filename('Spectrogram'))
 
-    def plot_title(self,title = ""):
+    def plot_title(self, title = ""):
         return 'Channel '+str(self.channel)+' '+title+'\n'+self.session_title
-        
+
     def plot_filename(self,title = ""):
         fn = self.session_title+' Channel '+str(self.channel)+' '+title
         valid_chars = '-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
         filename = 'plots/'+(''.join(c for c in fn if c in valid_chars)).replace(' ','_')+'.png'
         return filename
-        
 
-    def plot_spectrum_avg_fft(self):  
-        
+
+    def plot_spectrum_avg_fft(self):
+
         print("Generating power spectrum plot")
-              
+
         spectrum_PSDperHz = np.mean(self.spec_PSDperHz,1)
         plt.figure(figsize=(10,5))
         plt.plot(self.spec_freqs, 10*np.log10(spectrum_PSDperHz))  # dB re: 1 uV
@@ -221,16 +224,16 @@ class EEGrunt:
         plotname = 'Channel '+str(self.channel)+' Spectrum Average FFT Plot'
         plt.xlabel('Frequency (Hz)')
         plt.ylabel('PSD per Hz (dB re: 1uV^2/Hz)')
-        
+
         plt.title(self.plot_title("Power Spectrum"))
         self.plotit(plt, self.plot_filename("Power Spectrum"))
-        
-        
-    def plot_band_power(self,start_freq,end_freq,band_name):    
+
+
+    def plot_band_power(self,start_freq,end_freq,band_name):
         print("Plotting band power over time. Frequency range: "+str(start_freq)+" - "+str(end_freq))
         bool_inds = (self.spec_freqs > start_freq) & (self.spec_freqs < end_freq)
         band_power = np.sqrt(np.amax(self.spec_PSDperBin[bool_inds, :], 0))
-        plt.figure(figsize=(10,5))    
+        plt.figure(figsize=(10,5))
         plt.plot(self.spec_t,band_power)
         plt.ylim([np.amin(band_power), np.amax(band_power)+1])
         # plt.xlim(len(x)/config['sample_block'])
@@ -239,6 +242,61 @@ class EEGrunt:
         plt.title(self.plot_title('Trend Graph of '+band_name+' EEG Amplitude over Time'))
         self.plotit(plt, self.plot_filename(band_name+' EEG Amplitude Over Time'))
 
+
+    def plot_ecg(self):# segment
+        sig1 = self.data
+
+        print("Smoothing...")
+        sig1 = self.smooth(sig1,10)
+        sig1 = sig1[0:-9:] # Smoothing makes the signal longer, so we need to chop it off
+        sig1 = self.smooth(sig1,10)
+        sig1 = sig1[0:-9:] # Smoothing makes the signal longer, so we need to chop it off
+
+        signalDiff = np.diff(sig1)
+        signalDiff = np.append(signalDiff,0) # Cheap way to get shape to match...
+
+        # Handy for debugging:
+        # print signalDiff.shape
+        # print sig1.shape
+
+        absDiff = np.sqrt(signalDiff**2)
+        threshold = np.average(absDiff)*2
+
+        print("Threshold: " + str(threshold))
+
+        count = 0
+        lastVal = .0
+        currentRr = .0
+        # This array gets a value added for every sample, so can be plotted in the time domain
+        rrIntervalsArray = []
+        # This array is just RR values, useful for statistical purposes
+        rrIntervalsNotIndexedToSamples = []
+
+        for val in signalDiff:
+            count = count + 1
+
+            if (val > threshold and lastVal < threshold):
+                currentRr = (count/self.fs_Hz)
+                rrIntervalsNotIndexedToSamples.append(count)
+                count = 0
+            lastVal = val
+            rrIntervalsArray.append(currentRr)
+
+        print("Plotting ECG signal...")
+
+        plt.figure(figsize=(10,5))
+        plt.subplot(1,1,1)
+        plt.plot(self.t_sec,sig1/300)
+        plt.subplot(1,1,1)
+        plt.plot(self.t_sec,signalDiff/300)
+        plt.subplot(1,1,1)
+        plt.plot(self.t_sec,rrIntervalsArray)
+        plt.xlabel('Time (sec)')
+        plt.ylabel('RR Interval (sec)')
+        plt.title(self.plot_title('ECG Signal'))
+        plt.ylim(-1, 2)
+        plt.xlim(100, 500)
+        self.plotit(plt)
 
     def plot_coherence_fft(self, s1, s2, chan_a, chan_b):
         plt.figure()
