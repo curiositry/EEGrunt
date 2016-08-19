Fixed HRV standard deviation plot

EEGrunt.py
@@ -353,8 +353,9 @@ class EEGrunt:
 
         # Get the average heart-rate over the session (for the plot title)
         self.avgHeartRate = np.mean(heartRateArray)
+
         # Not sure how accurate this method of getting HRV is ...
-        self.sessionHRV = np.std(heartRateArray)
+        self.sessionHRV = np.std(self.rrIntervalsNotIndexedToSamples)
 
         plt.figure(figsize=(10,5))
         plt.subplot(1,1,1)
@@ -374,36 +375,52 @@ class EEGrunt:
             self.get_rr_intervals()
 
         hrvStdArray = []
-        arr = self.rrIntervalsNotIndexedToSamples
-
         index = 0
         errCount = 0
+        chunk = []
+
+        # For using time indexed, padded RR data
+        '''
+        arr = self.rrIntervalsArray
+        windowLength = 20
+        windowLengthSamples = int(windowLength*self.fs_Hz)
+        xLabel = "Samples"
+        '''
+
+        # Non-time-indexed unpadded RR data
+        arr = self.rrIntervalsNotIndexedToSamples
+        windowLength = 10
+        windowLengthSamples = int(windowLength*(self.avgHeartRate/60))
+        xLabel = "Heart beats"
+
+        print("Data length (samples):",len(arr))
+        print("Window length (samples):",windowLengthSamples)
+
         for val in arr:
-            if index > int(index+(5*self.fs_Hz)) and index < int(len(arr)-(5*self.fs_Hz)):
-                hrvStdValue = np.std(arr[index-(2.5*self.fs_Hz):index+(2.5*self.fs_Hz):])
-                hrvStdArray.append(hrvStdValue)
-            elif index <  int(index+(5*self.fs_Hz)):
-                hrvStdValue = np.std(arr[index:int(index+(5*self.fs_Hz)):])
-                hrvStdArray.append(hrvStdValue)
-            elif index > int(len(arr)-(5*self.fs_Hz)):
-                hrvStdValue = np.std(arr[len(arr)-(5*self.fs_Hz):len(arr):])
-                hrvStdArray.append(hrvStdValue)
+            if index < int(windowLengthSamples):
+                chunk = arr[:index:]
             else:
-                print("ERR")
+                chunk = arr[(index-windowLengthSamples):index:]
+
+            # print(chunk,index)
+
+            hrvStdValue = np.std(chunk)
+            hrvStdArray.append(hrvStdValue)
             index += 1
+
         dt = np.dtype('Float64')
         hrvStdArray = np.array(hrvStdArray, dtype=dt)
 
-
-        print("HRV array",hrvStdArray)
+        # print("HRV array",hrvStdArray)
         plt.figure(figsize=(10,5))
         plt.subplot(1,1,1)
-        plt.plot(hrvStdArray*50)
-        plt.subplot(1,1,1)
-        plt.plot(arr)
+        plt.plot(hrvStdArray)
 
-        plt.xlabel('X-axis label')
-        plt.ylabel('Standard deviation of R-R intervals (over 5-second window)')
+        # plt.subplot(1,1,1)
+        # plt.plot(arr)
+
+        plt.xlabel(xLabel)
+        plt.ylabel('Standard deviation of R-R intervals (over 5s window)')
         plt.title(self.plot_title('ECG Signal. \n Avg heart-rate: ' + str(int(self.avgHeartRate)) + "\n BPM. Standard deviation of R-R intervals over session (HRV): " + str(self.sessionHRV)))
         self.plotit(plt)
 